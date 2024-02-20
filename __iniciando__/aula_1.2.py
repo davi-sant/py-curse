0d1c3306168fa59788b92483b2b5c07f7a41fe05
Davi <davisantosdias@protonmail.com>
2024-02-19 22:11:46 -0300

    add print in python

---
+++ __iniciando__/aula_1.2.py
@@ -0,0 +1,12 @@
+#tipodedados #strings
+forma_05 = 5
+# forma 01
+print("forma 01")
+# forma 02
+print("foma '02'")
+# forma 03
+print('forma "03"')
+# forma 04
+print("forma \"04\"")
+# forma 05
+print(f"forma {forma_05}")
