b630766a829736ee60309e8d77bcde6470783614
Davi <davisantosdias@protonmail.com>
2024-02-20 00:27:23 -0300

    add type coercion

---
+++ __iniciando__/aula_1.5.py
@@ -0,0 +1,25 @@
+#typecoercion
+
+# Dado um input de dados faça uma soma qualquer se atentando para os tipo ultilizados. 
+nome = input("Digite seu nome: ")
+nota_01 = input("Digite sua nota: ")
+nota_02 = input("Digite a sua nota 02: ")
+
+
+class Student:
+    def __init__(self, nome: str, nota_01: float, nota_02: float):
+        self.nome = nome
+        self.nota_01 = nota_01
+        self.nota_02 = nota_02
+        
+    def create_student(nome: str, nota_01: float, nota_02: float):
+        
+        return Student(nome, nota_01, nota_02)
+
+student =  Student.create_student(nome, float(nota_01), float(nota_02))
+
+def avarege() -> print :
+    sum_avarege = (student.nota_01 + student.nota_02) / 2
+    print(f"Ola {student.nome} sua media é de {sum_avarege.real}")
+       
+avarege()
