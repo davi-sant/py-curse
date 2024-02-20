67aa71c6289b1fc636e6f306180377f0adf65aec
Davi <davisantosdias@protonmail.com>
2024-02-19 21:59:54 -0300

    Init curse from python

---
+++ __iniciando__/aula_1.0.py
@@ -0,0 +1,18 @@
+#comentarios
+
+# Mostre na tela uma messagem de terminal 
+print("Hallo Word !!!")
+
+
+# Comentarios de decumentação
+
+"""_summary_
+    Na linha a baixo vamos mostrar para o usuario a menssagem de hallo word.
+    print(str) -> Hallo Word
+"""
+print("Hallo Word 02 !!!")
+
+def teste(str: str) -> str:
+    return print(f"Ola eu {str}")
+
+teste("Davi")
---
+++ __iniciando__/aula_1.1.py
@@ -0,0 +1,11 @@
+#print
+
+'''__print__
+Nesta aula vamos falar sobre a função print, função essa que é nativa do python.
+O "print()" é usado para printar algum conteudo para o usuario 
+'''
+
+# Adicionando um separador na função print()
+
+print("Nesta", "linha", "vai", "ter", "separador", sep="-")
+print("Proxima linha")
